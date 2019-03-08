b946f31bebbd7fa6cfe3eabc2ce3a9f61abcd748
Anders Emil Salvesen <anders.emil.salvesen@visma.no>
2019-03-08 15:25:11 +0100

    Refining manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,13 +26,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Fagsamling med Visma Consulting`,
+        short_name: `fagsamling`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#007ACA`,
+        theme_color: `#007ACA`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/visma-just-logo.png`
       },
     },
     'gatsby-plugin-offline',
