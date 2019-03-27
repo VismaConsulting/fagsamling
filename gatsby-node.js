a17d95705257e3c8e9af728fe719b6adff66a9dd
Anders Emil Salvesen <anders.emil.salvesen@visma.no>
2019-03-27 08:07:31 +0100

    Forsinker onCreateNodes for å sørge for at alle eventer er laget

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,47 +6,56 @@
 
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+
+function addFieldsToMarkdown(node, getNode, getNodesByType, createNodeField, resolve) {
+    const basePath = `pages`;
+    const slug = createFilePath({node, getNode, basePath})
+    const subevents = node.frontmatter.subevents;
+    let subeventIds = [];
+    const markdownNodes = getNodesByType(`MarkdownRemark`);
+    console.log("# of markdown nodes", markdownNodes.length);
+    if (subevents) {
+        subeventIds = markdownNodes.filter(mdNode => {
+            const basePathLastLetterIndex = mdNode.fileAbsolutePath.indexOf(basePath) + basePath.length;
+            const fileRelativePath = mdNode.fileAbsolutePath.substring(basePathLastLetterIndex);
+            return subevents.filter(subevent => fileRelativePath.replace('.md', '/').includes(subevent)).length > 0;
+        }).map(mdNode => mdNode.id);
+        console.log("Number of subevents", subeventIds.length)
+    }
+    let speakers = [];
+    const speaker_links = node.frontmatter.speakers;
+    if (speaker_links) {
+        speakers = markdownNodes.filter(mdNode => {
+            const basePathLastLetterIndex = mdNode.fileAbsolutePath.indexOf(basePath) + basePath.length;
+            const fileRelativePath = mdNode.fileAbsolutePath.substring(basePathLastLetterIndex);
+            return speaker_links.filter(speaker => fileRelativePath.replace('.md', '/').includes(speaker)).length > 0
+        }).map(mdNode => mdNode.id);
+    }
+    createNodeField({
+        node,
+        name: `slug`,
+        value: slug,
+    })
+    createNodeField({
+        node,
+        name: 'subevents___NODE',
+        value: subeventIds
+    })
+    createNodeField({
+        node,
+        name: 'speakers___NODE',
+        value: speakers
+    })
+    resolve()
+}
+
 exports.onCreateNode = ({ node, getNode, getNodesByType, actions }) => {
     const { createNodeField } = actions
     if (node.internal.type === `MarkdownRemark`) {
-        const basePath = `pages`;
-        const slug = createFilePath({ node, getNode, basePath })
-        const subevents = node.frontmatter.subevents;
-        let subeventIds = [];
-        const markdownNodes = getNodesByType(`MarkdownRemark`);
-        console.log("# of markdown nodes", markdownNodes.length);
-        if (subevents) {
-            subeventIds = markdownNodes.filter(mdNode => {
-                const basePathLastLetterIndex = mdNode.fileAbsolutePath.indexOf(basePath) + basePath.length;
-                const fileRelativePath = mdNode.fileAbsolutePath.substring(basePathLastLetterIndex);
-                return subevents.filter(subevent => fileRelativePath.replace('.md', '/').includes(subevent)).length > 0;
-            }).map(mdNode => mdNode.id);
-            console.log("Number of subevents", subeventIds.length)
-        }
-        let speakers = [];
-        const speaker_links = node.frontmatter.speakers;
-        if (speaker_links) {
-            speakers = markdownNodes.filter(mdNode => {
-                const basePathLastLetterIndex = mdNode.fileAbsolutePath.indexOf(basePath) + basePath.length;
-                const fileRelativePath = mdNode.fileAbsolutePath.substring(basePathLastLetterIndex);
-                return speaker_links.filter(speaker => fileRelativePath.replace('.md', '/').includes(speaker)).length > 0
-            }).map(mdNode => mdNode.id);
-        }
-        createNodeField({
-            node,
-            name: `slug`,
-            value: slug,
-        })
-        createNodeField({
-            node,
-            name: 'subevents___NODE',
-            value: subeventIds
-        })
-        createNodeField({
-            node,
-            name: 'speakers___NODE',
-            value: speakers
-        })
+        return new Promise((resolve, reject) => {
+            setTimeout(addFieldsToMarkdown, 1500, node, getNode, getNodesByType, createNodeField, resolve);
+        });
+
     }
 }
 
